72746e27c0b051c2d1b9e1929b537e486567dcc6
ZidanMusk <mahmoud.ismail.zidan@gmail.com>
2017-03-01 00:10:52 +0200

    changed (for episodes) to (while step)

--- mainTrain.py
+++ mainTrain.py
@@ -8,9 +8,7 @@ from settings import AgentSetting
 
 #gym env name
 ENV_NAME = 'Breakout-v0'
-#max episodes to run 
-#TODO Deprecate it
-MAX_EPISODES = 10000
+
 #save model every ? episodes
 SAVE_EVERY = 5 
 #max update steps to run
@@ -43,34 +41,37 @@ def main():
 
 		print("Starting training at Episode {} and Step {}...".format(episodeNum.eval(),step.eval()))
 		
-		#TODO deprecate looping on episodes
-		for ep in tqdm(xrange(MAX_EPISODES)):# for episodes
-			
-			cumulatedSteps = step.eval()
-			curEpNum = episodeNum.eval() 
-			
-			dqn.learning(sess) #returns when an episode is done
-			
-			print ('Finished Episode no. %d  in %.3f secs...with ::' % (curEpNum, dqn.duration))
-			print('Ep.Steps %d...total Ep.Reward = %.2f, total Ep.Loss = %.4f' % (step.eval()-cumulatedSteps, dqn.totalReward, dqn.totalLoss))
-			print('Trained for : %d steps and %d episodes in %.3f  hrs' %(step.eval(), curEpNum+1, dqn.training_hrs))			
-			
-			#saves summmaries for tensorboard
-			dqn.util.summary_board(sess,step.eval(),forTrain = TRAINING)
-			
-			if ((ep % SAVE_EVERY == 0) or ((ep + 1) == MAX_EPISODES)):
 
-				#saves all tf graph nodes, and if(save2play) saves online weights for evaluation(via mainPlay.py)
-				dqn.util.save_graph(sess,step.eval(),save2play = True)
-				#print('Trained for : %d steps and  %.3f  hrs' %(step.eval(),dqn.training_hrs))
-
-			if (step.eval() >= MAX_STEPS):
-
-				dqn.util.save_graph(sess,step.eval(),save2play = True)
-				print('Finished Training for %.0f Frames!...took me %.3f hrs...Now run mainPlay.py and watch me play :)'%(step.eval()* AgentSetting.update_freq,dqn.training_hrs))
-				break
+		with tqdm(initial = step.eval(),total= MAX_STEPS) as curStep:
 			
+			while (step.eval() < MAX_STEPS):
 
+				cumulatedSteps = step.eval()
+				curEpNum = episodeNum.eval() 
+				
+				dqn.learning(sess) #returns when an episode is done
+				
+				curStep.update(step.eval())
+
+				print ('Finished Episode no. %d  in %.3f secs...with ::' % (curEpNum, dqn.duration))
+				print('Ep.Steps %d...total Ep.Reward = %.2f, total Ep.Loss = %.4f' % (step.eval()-cumulatedSteps, dqn.totalReward, dqn.totalLoss))
+				print('Trained for : %d steps and %d episodes in %.3f  hrs' %(step.eval(), curEpNum+1, dqn.training_hrs))			
+				
+				#saves summmaries for tensorboard
+				dqn.util.summary_board(sess,step.eval(),forTrain = TRAINING)
+				
+				#every some episodes save 
+				if ((curEpNum+1) % SAVE_EVERY == 0):
+
+					#saves all tf graph nodes, and if(save2play) saves online weights for evaluation(via mainPlay.py)
+					dqn.util.save_graph(sess,step.eval(),save2play = True)
+
+				if (step.eval() >= MAX_STEPS):
+
+					dqn.util.save_graph(sess,step.eval(),save2play = True)
+					print('Finished Training for %.0f Frames!...took me %.3f hrs...Now run mainPlay.py and watch me play :)'%(step.eval()* AgentSetting.update_freq,dqn.training_hrs))
+					
+				
 			
 #RUN...
 main()
