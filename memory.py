477c61dfd004188d7615cd36ce09844bb1069df8
Mahmoud Zidan <mahmoud.zidan95@eng-st.cu.edu.eg>
2018-02-25 04:53:43 +0200

    Update memory.py

--- memory.py
+++ memory.py
@@ -5,12 +5,12 @@ import random
 class ExperienceMemory(object):
 
     def __init__(self, state_shape ,buffer_size = 50000): # buff size = no. of exp tuples
-
-        self.stateMem = np.empty([buffer_size,state_shape[0],state_shape[1],state_shape[2]])
-        self.actionMem = np.empty([buffer_size],dtype = int)
-        self.rewardMem = np.empty([buffer_size], dtype= float)
-        self.doneMem = np.empty([buffer_size],dtype= bool)
-        self.nxt_stateMem = np.empty([buffer_size,state_shape[0],state_shape[1],state_shape[2]])
+        buffer_size_init = 100000
+        self.stateMem = np.empty([buffer_size_init,state_shape[0],state_shape[1],state_shape[2]])
+        self.actionMem = np.empty([buffer_size_init],dtype = int)
+        self.rewardMem = np.empty([buffer_size_init], dtype= float)
+        self.doneMem = np.empty([buffer_size_init],dtype= bool)
+        self.nxt_stateMem = np.empty([buffer_size_init,state_shape[0],state_shape[1],state_shape[2]])
 
         self.buffer_size = buffer_size
         self.isFull = False
